2889f1501a7bba1ff1654d9c291c443271b5f37c
Erika Secillano <C15339871@mydit.ie>
2018-01-04 01:07:47 +0100

    Oxygen Bar working andHealth Bar Working
    Oxygen Bar now decreases by 20 every second it runs. If Oxygen Bar goes 0, health bar decreases by 1.  :+1:

---
+++ Project2/Assets/Script/PlayerManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerManager : MonoBehaviour {
+
+    //Health will be 100 percent
+    public static int health = 100;
+
+    //oxygen 
+    public static int oxygen = 100;
+
+    //Slider for oxygen and health bar
+    public Slider OxygenBar;
+    public Slider HealthBar;
+
+    // Use this for initialization
+    /// <summary>
+    /// It repeats every 1 second
+    /// </summary>
+    void Start () {
+        InvokeRepeating("ReduceVitals", 1, 1);
+	}
+	
+	void ReduceVitals()
+    {
+        oxygen = oxygen - 20;
+        OxygenBar.value = oxygen;
+        HealthBar.value = health;
+        if (oxygen <= 0)
+        {
+            health--;
+        }
+        if (health <= 0)
+        {
+            print("You are dead");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
