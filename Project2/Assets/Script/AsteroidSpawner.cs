200cd2cff550723e85dbdd72a4ec304ffccafa5b
Butan <c15487922@mydit.ie>
2018-02-13 07:06:26 +0100

    functionality of the traps added
    self destruct, spawner and its event trigger

---
+++ Project2/Assets/Script/AsteroidSpawner.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject[] asteroids;
+    public Vector3 spawnValues;
+    public float spawnWait;
+    public float spawnMostWait;
+    public float spawnLeastWait;
+    public int startWait;
+    public bool end;
+    int randomAsteroids;
+
+    void Start()
+    {
+        StartCoroutine(waitSpawner());
+    }
+
+    void Update()
+    {
+        spawnWait = Random.Range(spawnLeastWait, spawnMostWait);
+    }
+
+    IEnumerator waitSpawner()
+    {
+        yield return new WaitForSeconds(startWait);
+
+        while (!end)
+        {
+            randomAsteroids = Random.Range(0, 2);
+            Vector3 spawnPosition = new Vector3(1, Random.Range(-spawnValues.y, spawnValues.y), Random.Range(-spawnValues.z, spawnValues.z));
+            Instantiate(asteroids[randomAsteroids], spawnPosition + transform.TransformPoint(0, 0, 0), gameObject.transform.rotation);
+            yield return new WaitForSeconds(spawnWait);
+        }
+
+    }
+
+}
