c7cbf91340ab1d1ad1ca9db11d924dd47482600b
Erika Secillano <C15339871@mydit.ie>
2018-02-18 17:13:37 +0100

    attempt in singleton for Health and Oxygen Bar

--- Project2/Assets/Script/PlayerManager.cs
+++ Project2/Assets/Script/PlayerManager.cs
@@ -1,11 +1,16 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class PlayerManager : MonoBehaviour {
 
-    int time = 10;
+    //
+    public static PlayerManager instance = null;
+
+    //
+    public int time = 10;
 
     //Health will be 100 percent
     public static int health = 100;
@@ -14,35 +19,37 @@ public class PlayerManager : MonoBehaviour {
     public static int oxygen = 100;
 
     //Slider for oxygen and health bar
-    public Slider OxygenBar;
     public Slider HealthBar;
+    public Slider OxygenBar;
 
     // Use this for initialization
     /// <summary>
     /// It repeats every 1 second
     /// </summary>
-    void Start () {
-        InvokeRepeating("ReduceVitals", time, time);
+    void Awake () {
+        if (instance != null && instance != this)
+            Destroy(gameObject);    // Ensures that there aren't multiple Singletons
+
+        instance = this;
+        PlayerManager.instance.InvokeRepeating("Vitals", time, time);
 	}
 	
-	void ReduceVitals()
+	public class Vitals
     {
-        oxygen = oxygen - 5;
-        OxygenBar.value = oxygen;
-        HealthBar.value = health;
-        if (oxygen <= 0)
-        {
-            health = health - 15;
-        }
-        if (health <= 0)
-        {
-            print("You are dead");
-        }
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        void reduceVitals()
+        {
+            oxygen = oxygen - 5;
+            PlayerManager.instance.OxygenBar.value = oxygen;
+            PlayerManager.instance.HealthBar.value = health;
+            if (oxygen <= 0)
+            {
+                health = health - 15;
+            }
+            if (health <= 0)
+            {
+                print("You are dead");
+            }
+        }
     }
 }
