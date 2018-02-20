b2da3e3145c0035b036fc12b6d9206255cc276cd
Pia Ofalsa <c15734155@mydit.ie>
2018-02-20 19:45:17 +0100

    unit testing spawninh
    unit testing progress

--- Project2/Assets/Script/EnemySpawner.cs
+++ Project2/Assets/Script/EnemySpawner.cs
@@ -11,32 +11,32 @@ namespace Assets.Script
         [SerializeField] private int _radius;
 
         private float _timeSinceLastSpawn;
+        private Circle _circle;
         public Random Random { get; set; }
 
         public void Construct(Object enemyPrefab, float spawnRate,int radius)
         {
             _enemyPrefab = enemyPrefab;
             _spawnRate = spawnRate;
-            _radius = radius;
-
+            _circle= new Circle(radius);
+           
         }
         // Update is called once per frame
-        private void Update () {
+        public void Update () {
+
+            if(_circle ==null)
+                _circle = new Circle(_radius);
+
 
             if (Random == null)
                 Random = new Random();
+
             if(_timeSinceLastSpawn >= _spawnRate)
             {
+                //create enemy from prefab
                 var enemy = PrefabUtility.InstantiatePrefab(_enemyPrefab) as GameObject;
-                var degrees = Random.Next(0, 361);
 
-                var x = _radius * Mathf.Cos(degrees * Mathf.Deg2Rad);
-                if (Mathf.Abs(x) < 0.01f) x = 0;
-
-                var y = _radius * Mathf.Cos(degrees * Mathf.Deg2Rad);
-                if (Mathf.Abs(y) < 0.01f) y = 0;
-
-                enemy.transform.position = new Vector3(x, 0, y);
+                enemy.transform.position = _circle.GetPositionOnBoundaryOfCircle(Random.Next(0, 361));
                 _timeSinceLastSpawn = 0;
             }
             _timeSinceLastSpawn += Time.deltaTime;
---
+++ Project2/Assets/Tests/SpawningTest.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using Assets.Script;
+using NSubstitute;
+using NUnit.Framework;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Assets.Tests
+{
+    public class SpawningTest
+    {
+
+        [UnityTest]
+        public IEnumerator _Instantiantes_GameObject_From_Prefab()
+        {
+            var enemyPrefab = Resources.Load("Tests/Enemy");
+            var enemySpawner = new GameObject().AddComponent<EnemySpawner>();
+
+            //construct method will setup state
+            enemySpawner.Construct(enemyPrefab, 0, 1);
+
+            yield return null;
+
+            var spawnedEnemy = GameObject.FindWithTag("Enemy");
+            var prefabOfTheSpawnedEnemy = PrefabUtility.GetPrefabParent(spawnedEnemy);
+
+            Assert.AreEqual(enemyPrefab, prefabOfTheSpawnedEnemy);
+        }
+
+        [UnityTest]
+        public IEnumerator _Instantiates_GameObject_At_Random_Position_On_Circle_Boundary()
+        {
+            var enemyPrefab = Resources.Load("Tests/enemy");
+            var enemySpawner = new GameObject().AddComponent<EnemySpawner>();
+            //construct method will setup state
+            enemySpawner.Construct(enemyPrefab, 0, 1);
+            var random = NSubstitute.Substitute.For<System.Random>();
+            random.Next(Arg.Any<int>(), Arg.Any<int>()).Returns(270);
+            enemySpawner.Random = random;
+
+
+            yield return null;
+
+            var spawnedEnemy = GameObject.FindWithTag("Enemy");
+            //270 degrees
+            var expectedPosition = new Vector3((float)-6.5, (float)1.1, (float)5.7);
+
+            Assert.AreEqual(expectedPosition, spawnedEnemy.transform.position);
+
+
+
+        }
+
+        [UnityTest]
+        public IEnumerator _Instantiations_Occur_On_An_Interval()
+        {
+
+            var enemyPrefab = Resources.Load("Tests/enemy");
+            var enemySpawner = new GameObject().AddComponent<EnemySpawner>();
+
+            //construct method will setup state
+            enemySpawner.Construct(enemyPrefab, 1, 1);
+
+            yield return new WaitForSeconds(0.75f);
+            var spawnedEnemy = GameObject.FindWithTag("Enemy");
+
+            Assert.IsNull(spawnedEnemy);
+        }
+
+        //will be run after test
+        [TearDown]
+        public void AfterEveryTest()
+        {
+            foreach (var gameObject in GameObject.FindGameObjectsWithTag("Enemy"))
+                Object.Destroy(gameObject);
+
+            foreach (var gameObject in Object.FindObjectsOfType<EnemySpawner>())
+                Object.Destroy(gameObject);
+
+        }
+    }
+}
