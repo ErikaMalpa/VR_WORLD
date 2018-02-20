878059f6d1f27047498fa866226b623314225823
Butan <c15487922@mydit.ie>
2018-02-20 20:33:38 +0100

    Polished game manager

---
+++ Project2/Assets/Script/Managers/GameManager/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+    bool gameHasEnded = false;
+    public float restartDelay = 2f;
+
+	public void EndGame()
+    {
+        if (gameHasEnded == false)
+        {
+            gameHasEnded = true;
+            Debug.Log("GAME OVER");
+            Invoke("Restart", restartDelay);
+        }
+
+    }
+
+    void Restart()
+    {
+        SceneManager.LoadScene("MainScreen");
+
+    }
+}
