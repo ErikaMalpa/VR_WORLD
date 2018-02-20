0e5e4c253e90d61bddd495b39d4ee97b1d5a3023
Pia Ofalsa <c15734155@mydit.ie>
2018-02-20 03:52:21 +0100

    spawn enemy code and unit test
    testing simple enemy spawn

---
+++ Project2/Assets/Script/EnemySpawner.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+using Random = System.Random;
+
+public class EnemySpawner : MonoBehaviour {
+
+    [SerializeField] private Object _enemyPrefab;
+    [SerializeField] private float _spawnRate;
+    [SerializeField] private int _radius;
+
+    private float _timeSinceLastSpawn;
+    public Random Random { get; set; }
+
+    public void Construct(Object enemyPrefab, float spawnRate,int radius)
+    {
+        _enemyPrefab = enemyPrefab;
+        _spawnRate = spawnRate;
+        _radius = radius;
+
+    }
+    // Update is called once per frame
+    private void Update () {
+
+        if (Random == null)
+            Random = new Random();
+        if(_timeSinceLastSpawn >= _spawnRate)
+        {
+            var enemy = PrefabUtility.InstantiatePrefab(_enemyPrefab) as GameObject;
+            var degrees = Random.Next(0, 361);
+
+            var x = _radius * Mathf.Cos(degrees * Mathf.Deg2Rad);
+            if (Mathf.Abs(x) < 0.01f) x = 0;
+
+            var y = _radius * Mathf.Cos(degrees * Mathf.Deg2Rad);
+            if (Mathf.Abs(y) < 0.01f) y = 0;
+
+            enemy.transform.position = new Vector3(x, 0, y);
+            _timeSinceLastSpawn = 0;
+        }
+        _timeSinceLastSpawn += Time.deltaTime;
+	}
+}
---
+++ Project2/Assets/Tests/Editor/EnemySpawnerTests.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using NUnit.Framework;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Assets.Tests.Editor
+{
+    public class EnemySpawnerTests {
+     
+
+        //[Test]
+        //public void EnemySpaswnerTestsSimplePasses() {
+        //	// Use the Assert class to test conditions.
+        //}
+
+        //// A UnityTest behaves like a coroutine in PlayMode
+        //// and allows you to yield null to skip a frame in EditMode
+        //[UnityTest]
+        //public IEnumerator EnemySpaswnerTestsWithEnumeratorPasses() {
+        //	// Use the Assert class to test conditions.
+        //	// yield to skip a frame
+        //	
+        //}
+
+        [UnityTest]
+        public IEnumerator _Instantiantes_GameObject_From_Prefab()
+        {
+            var enemyPrefab = Resources.Load("Tests/enemy");
+            var enemySpawner = new GameObject().AddComponent<EnemySpawner>();
+        
+            //construct method will setup state
+            enemySpawner.Construct(enemyPrefab,0,1);
+
+            yield return null;
+
+            var spawnedEnemy = GameObject.FindWithTag("Enemy");
+            var prefabOfTheSpawnedEnemy = PrefabUtility.GetPrefabParent(spawnedEnemy);
+
+            Assert.AreEqual(enemyPrefab, prefabOfTheSpawnedEnemy);
+        }
+
+        [UnityTest]
+        public IEnumerator _Instantiates_GameObject_At_Random_Position_On_Circle_Boundary()
+        {
+            var enemyPrefab = Resources.Load("Tests/enemy");
+            var enemySpawner = new GameObject().AddComponent<EnemySpawner>();
+            //construct method will setup state
+            enemySpawner.Construct(enemyPrefab, 0, 1);
+            var random = NSubstitute.Substitute.For<System.Random>();
+            random.Next(Arg.Any<int>(),Arg.Any<int>()).Returns(270);
+            enemySpawner.Random = random;
+
+
+            yield return null;
+
+            var spawnedEnemy = GameObject.FindWithTag("Enemy");
+            //270 degrees
+            var expectedPosition = new Vector3(0, 0, -1);
+
+            Assert.AreEqual(expectedPosition,spawnedEnemy.transform.position);
+        
+        
+
+        
+        }
+
+        //will be run after test
+        [TearDown]
+        public void AfterEveryTest()
+        {
+            foreach (var gameObject in GameObject.FindGameObjectsWithTag("Enemy"))
+                Object.Destroy(gameObject);
+
+            foreach (var gameObject in Object.FindObjectsOfType<EnemySpawner>())
+                Object.Destroy(gameObject);
+
+        }
+    }
+}
