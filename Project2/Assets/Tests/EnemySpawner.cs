8691bb55cf03bbc0ca94c2bf12c43eca8de58906
Butan <c15487922@mydit.ie>
2018-02-23 22:23:14 +0100

    added the tests back

---
+++ Project2/Assets/Tests/EnemySpawner.cs
@@ -0,0 +1,47 @@
+﻿using UnityEditor;
+using UnityEngine;
+using Random = System.Random;
+
+namespace Assets.Tests
+{
+    using Assets.Script;
+
+    public class EnemySpawner : MonoBehaviour {
+
+        [SerializeField] private Object _enemyPrefab;
+        [SerializeField] private float _spawnRate;
+        [SerializeField] private int _radius;
+
+        private float _timeSinceLastSpawn;
+        private Circle _circle;
+        public Random Random { get; set; }
+
+        public void Construct(Object enemyPrefab, float spawnRate,int radius)
+        {
+            _enemyPrefab = enemyPrefab;
+            _spawnRate = spawnRate;
+            _circle= new Circle(radius);
+           
+        }
+        // Update is called once per frame
+        public void Update () {
+
+            if(_circle ==null)
+                _circle = new Circle(_radius);
+
+
+            if (Random == null)
+                Random = new Random();
+
+            if(_timeSinceLastSpawn >= _spawnRate)
+            {
+                //create enemy from prefab
+                var enemy = PrefabUtility.InstantiatePrefab(_enemyPrefab) as GameObject;
+
+                enemy.transform.position = this._circle.GetPositionOnBoundaryOfCircle(Random.Next(0, 361));
+                _timeSinceLastSpawn = 0;
+            }
+            _timeSinceLastSpawn += Time.deltaTime;
+        }
+    }
+}
